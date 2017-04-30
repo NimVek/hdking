8673d2fc011516c1025bf863a971caa6bb4946f2
Nim'Vek <nimvek@web.de>
2017-04-30 12:01:18 +0200

    Mockup some basic stuff

---
+++ hdking/HDKingShared.py
@@ -0,0 +1,13 @@
+import logging
+
+LOG_LEVEL = logging.DEBUG
+logging.basicConfig()
+log = logging.getLogger('hdking')
+log.setLevel(LOG_LEVEL)
+
+class HDKingException(Exception):
+    pass
+
+def HDKingAssert(condition, msg):
+    if not condition:
+	raise HDKingException(msg)
